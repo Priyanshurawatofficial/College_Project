e39a7bbdadeace3c0a2c181936a830af9653fab3
Priyanshurawatofficial <priyanshurawatmaster@gmail.com>
2025-07-09 20:22:15 +0530

    error fixes

--- backend/index.js
+++ backend/index.js
@@ -20,8 +20,9 @@ mongoose.connect(Mongo_Url)
 
 
 app.use('/', controller);
-app.listen(3000, () => {
-  console.log("Server is started on port 3000");
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is started on port ${PORT}`);
 });
 
 
