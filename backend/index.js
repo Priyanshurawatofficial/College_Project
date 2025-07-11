1b4aef544799e023d97c259f9bb01f75289323d0
Priyanshurawatofficial <priyanshurawatmaster@gmail.com>
2025-07-11 22:26:05 +0530

    ..

--- backend/index.js
+++ backend/index.js
@@ -10,9 +10,9 @@ const path=require("path")
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+// });
 
 // Health check endpoint
 app.get('/', (req, res) => {
