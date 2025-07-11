2bba6df50e4211ad81562f981828327d101af57b
Priyanshurawatofficial <priyanshurawatmaster@gmail.com>
2025-07-11 22:15:03 +0530

    still fixing refreshing error

--- backend/index.js
+++ backend/index.js
@@ -5,9 +5,14 @@ const app = express();
 const controller = require('./controllers/Controller');
 const Mongo_Url = process.env.Mongo_Url;
 const cors=require("cors");
-app.use(cors());
+const path=require("path")
 
+app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'dist')));
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
 
 // Health check endpoint
 app.get('/', (req, res) => {
